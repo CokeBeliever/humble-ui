d8179883db20242df638ba38260f0513dc61e6e1
CokeBeliever <cokebeliever@163.com>
2022-11-29 23:51:32 +0800

    feat(build-constants): init @humble-ui/build-constants package

---
+++ internal/build-constants/src/pkg.ts
@@ -0,0 +1,5 @@
+export const PKG_PREFIX = '@humble-ui'
+export const PKG_NAME = 'humble-ui'
+export const PKG_CAMELCASE_NAME = 'HumbleUI'
+export const PKG_CAMELCASE_LOCAL_NAME = 'HumbleUILocale'
+export const PKG_BRAND_NAME = 'Humble UI'
