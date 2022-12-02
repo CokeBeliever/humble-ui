e65195e3d10eccbe295410a2140366cdd6778242
CokeBeliever <cokebeliever@163.com>
2022-12-02 21:58:44 +0800

    chore(build-constants): change REPO_OWNER value

--- internal/build-constants/src/repo.ts
+++ internal/build-constants/src/repo.ts
@@ -1,4 +1,4 @@
-export const REPO_OWNER = 'humble-ui'
+export const REPO_OWNER = 'CokeBeliever'
 export const REPO_NAME = 'humble-ui'
 export const REPO_PATH = `${REPO_OWNER}/${REPO_NAME}`
 export const REPO_BRANCH = 'master'
